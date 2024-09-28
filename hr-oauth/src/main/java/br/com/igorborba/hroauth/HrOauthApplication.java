8e118e51da0b64c97087ffa237d18178fea21f20
igorcamposdeborba <igor_campos5@hotmail.com>
2024-09-28 11:11:56 -0300

    Configured oauth to api gateway

--- hr-oauth/src/main/java/br/com/igorborba/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/br/com/igorborba/hroauth/HrOauthApplication.java
@@ -2,8 +2,10 @@ package br.com.igorborba.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Habilitar eureka client para se conectar ao eureka server para encontrar as portas de cada projeto baseado no nome
 public class HrOauthApplication {
 
     public static void main(String[] args) {
